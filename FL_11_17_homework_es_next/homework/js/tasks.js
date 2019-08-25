0915c4eca3c9b211916073c9083cbac3e56fb135
Nataliya Kuziv <neti.kuzja@gmail.com>
2019-08-25 23:15:29 +0300

    homework 17: Replased wiht arrow functions.

--- FL_11_17_homework_es_next/homework/js/tasks.js
+++ FL_11_17_homework_es_next/homework/js/tasks.js
@@ -1,17 +1,11 @@
 /* Task 1 */
-function maxElement(arr) {
-    return Math.max(...arr);
-    // return Math.max(...arr);
-}
+const maxElement = (arr) => Math.max(...arr);
 
 const array0 = [1,2,3,4,56,7,8,76,5,241,5,356,567,2];
 console.log(maxElement(array0));
 
 /* Task 2 */
-function copyArray(arr) {
-    const copArray = [...arr];
-    return copArray;
-}
+const copyArray = (arr) => [...arr];
 
 const array = [1,2,3];
 const copiedArray = copyArray(array);
@@ -19,37 +13,28 @@ console.log(array, copiedArray);
 console.log(array === copiedArray);
 
 /* Task 3 */
-function addUniqueId(obj){
-    return {id: Symbol(), ...obj};
-}
+const addUniqueId = (obj) => {return {id: Symbol(), ...obj};};
+
 console.log(addUniqueId({name: 123}));
 
-/* Task 4 !!!!!!!!!!!!!!!!!!!!!!!*/
+/* Task 4 */
+const regroupObject = (oldObj) => ({...oldObj});
+
 const oldObj = {name: 'Someone', detalis: {id: 1, age: 11, university: 'UNI'}};
-const newObj = {university: 'UNI', user: {age: 11, firstName: 'Someone', id: 1}};
-// console.log(oldObj);
-// console.log(newObj);
-
-function regroupObject() {
-    return newObj;
-}
-
 console.log(regroupObject());
 
 /* Task 5 */
-function findUniqueElement(arr) {
+const findUniqueElement = (arr) => {
     const unigSet = new Set(arr);
     return Array.from(unigSet);
-}
+};
 
 const digitsArray = [1,1,23,3,4,5,6,5,4,23,2,1,1,1,1,1];
 console.log(digitsArray);
 console.log(findUniqueElement(digitsArray));
 
 /* Task 6 */
-function hideNumber(str) {
-    return str.slice(-4).padStart(10, '*');
-}
+const hideNumber = (str) => {return str.slice(-4).padStart(10, '*');};
 
 const phoneNumber = '0123456789';
 console.log(hideNumber(phoneNumber));
