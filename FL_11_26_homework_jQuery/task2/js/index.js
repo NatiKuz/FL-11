cc276fe42e010870347afa36f3eb88e08c03773b
Nataliya Kuziv <neti.kuzja@gmail.com>
2019-09-25 00:48:47 +0300

    Fixed bugs in task 2.

--- FL_11_26_homework_jQuery/task2/js/index.js
+++ FL_11_26_homework_jQuery/task2/js/index.js
@@ -3,12 +3,12 @@ const $input  = $("#add-input");
 const $add = $("#add-submit");
 
 //generate a new list item
-let makeListItem = function(task, status='false') {
+let makeListItem = function({name, status, id}) {
 
-  let $listItem = $(`<li class="item" data-status="${false}"
-    <span class="item-text">${task}</span>
+  let $listItem = $(`<li class="item" data-status="${status}" data-id="${id}">
+    <span class="item-text ${status ? "done" : ""}">${name}</span>
     <button class="item-remove">Remove</button>
-    <button class="item-status">In Progress</button>
+    <button class="item-status">${status ? "Done" : "In Progress"}</button>
   </li>`);
   
   return $listItem;
@@ -18,39 +18,85 @@ $add.on( "click", function(event) {
   event.preventDefault();
 
   const newTask = $input.val();
-  let listItemToAdd = makeListItem(newTask);
-  $list.append(listItemToAdd);
-  let todos = JSON.parse(localStorage.todos || '[]');
+
+  let todos = getTodos();
   let maxId = todos.reduce((acc, cur) => {
     if (acc < cur.id) {
       acc = cur.id;
     }
 
     return acc;
-  }, 0)
+  }, 0);
   todos.push({ id: ++maxId, name: newTask, status: false })
-  localStorage.todos = JSON.stringify(todos);
+  setTodos(todos)
+
+  let listItemToAdd = makeListItem({name: newTask, status: false, id: maxId});
+  $list.append(listItemToAdd);
 
   $input.val("");
+  calculate();
 });
 
-$list.on( "click", ".item-remove", function() {
+$list.on( "click", ".item-remove", function(event) {
+  event.preventDefault();
+  let id = parseInt($(this).closest(".item").attr("data-id"), 10);
+  let todos = getTodos();
+  todos = todos.filter(item => item.id !== id);
+  setTodos(todos)
+
   $(this).parent().remove();
+  calculate();
 });
 
 $list.on( "click", ".item-status", function(event) {
-  console.log("event = ", event)
-  $(this).parent().prop('data-status', );
+  let id = parseInt($(this).closest(".item").attr("data-id"), 10);
+
+  let todos = getTodos();
+  todo = todos.find(item => item.id === id);
+
+  const status = !todo.status;
+  todo.status = status;
+
+  $(this).closest(".item").attr("data-status", status);
+  $(this).siblings("span").toggleClass("done");
+  $(this).text(status ? "Done" : "In progress");
+  
+  setTodos(todos);
+  calculate();
 });
 
 function generateList($list) {
-  const todos = JSON.parse(localStorage.todos || '[]');
+  const todos = JSON.parse(localStorage.todos || "[]");
   $list.innerHTML = "";
   
   for (const todo of todos) {
-      $list.append(makeListItem(todo.name, todo.status));
+      $list.append(makeListItem(todo));
 
   }
+
+  calculate();
+}
+
+function getTodos() {
+  return JSON.parse(localStorage.todos || "[]");
+}
+
+function setTodos(todos) {
+  localStorage.todos = JSON.stringify(todos);
+}
+
+function calculate() {
+  const $done = $(".items-done");
+  const $inProgress = $(".in-progress");
+  const $total = $(".total");
+
+  const todos = getTodos();
+  const done = todos.filter(item => item.status).length;
+  const inProgress = todos.filter(item => !item.status).length;
+
+  $done.text(done);
+  $inProgress.text(inProgress);
+  $total.text(done + inProgress);
 }
 
 $(document).ready(function() {
